61d578f7a37f228a68053c6233a878aaa6f194c5
RichardLiao <bnmghjtyu0@gmail.com>
2020-09-07 15:59:25 +0800

    加入 autolink

--- src/app_html.js
+++ src/app_html.js
@@ -2,6 +2,7 @@
 
 // import "tinymce/plugins/link";
 // import "tinymce/plugins/lists";
+require("tinymce-mention");
 
 let status = {
   bold: false,
@@ -44,22 +45,31 @@ const CORE_CSS = `
 window.init = (config) => {
   tinymce.init({
     selector: "#editor",
-    toolbar: "bold italic | link image | myCustomToolbarButton",
+    // toolbar: "bold italic | link image | myCustomToolbarButton",
+    toolbar: "bold italic myCustomToolbarButton",
+    toolbar_location: "bottom",
+    plugins: ["mentions", "autolink","link"],
     font_formats:
       "Arial=arial,helvetica,sans-serif; Courier New=courier new,courier,monospace; AkrutiKndPadmini=Akpdmi-n",
     // Remove all UI.
     menubar: false,
     statusbar: false,
-    theme_advanced_toolbar_location: "bottom",
+    mentions: {
+      source: [
+        { name: "Tyra Porcelli" },
+        { name: "Brigid Reddish" },
+        { name: "Ashely Buckler" },
+        { name: "Teddy Whelan" },
+      ],
+      delimiter: ["@", "#"],
+    },
+
     // Reset content styles.
     // content_css: false,
     // content_style: CORE_CSS + ( config.content_style || '' ),
 
     // No need for inputs.
     // hidden_input: false,
-
-    // Add some basic plugins.
-    // plugins: ["link", "lists"]
 
     // 選擇文字後，會跳出 popup
     // setup: function (editor) {
@@ -91,7 +101,13 @@ window.init = (config) => {
         },
       });
     },
+
     content_style:
       "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+
+    //   autolink
+    // link_default_protocol: "https",
+    // default_link_target: "_blank",
   });
+  tinymce.activeEditor.execCommand("mceAnchor");
 };
